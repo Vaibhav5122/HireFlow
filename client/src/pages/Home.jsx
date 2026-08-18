6dd458f20a4798bec67a55976bef62abac424a79
Vaibhav5122 <vwaghmode5757@gmail.com>
2026-08-18 09:46:46 +0530

    feat: add backend wake-up functionality with environment variable check

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import JobListings from "../components/JobListings";
@@ -6,6 +6,20 @@ import AppDownload from "../components/AppDownload";
 import Footer from "../components/Footer";
 
 const Home = () => {
+  // Silent "wake up" request to Render backend
+  const prodBackendURL = import.meta.env.VITE_BACKEND_URL;
+
+  useEffect(() => {
+    if (!prodBackendURL) {
+      console.warn("VITE_BACKEND_URL variable is missing!");
+      return;
+    }
+
+    fetch(prodBackendURL)
+      .then(() => console.log("Backend waking up..."))
+      .catch((err) => console.error("Wake up failed", err));
+  }, [prodBackendURL]);
+
   return (
     <div>
       <Navbar />
